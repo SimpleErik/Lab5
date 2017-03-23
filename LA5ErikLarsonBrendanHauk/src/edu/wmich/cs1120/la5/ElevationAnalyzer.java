063dfa2c2e3c8379e270392c4edaa62f1822bb3c
eriklarson <eriklarson@wireless140-049.wireless.wmich.edu>
2017-03-23 15:52:24 -0400

    changes to elevation analyzer

--- LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/ElevationAnalyzer.java
+++ LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/ElevationAnalyzer.java
@@ -3,23 +3,28 @@ package edu.wmich.cs1120.la5;
 import java.util.ArrayList;
 
 public class ElevationAnalyzer implements IRover {
-
+	private ArrayList<IArea> path = new ArrayList<IArea>();
+	private String analysis;
 	@Override
 	public ArrayList<IArea> getPath() {
 		// TODO Auto-generated method stub
-		return null;
+		return path;
 	}
 
 	@Override
 	public void setPath(ArrayList<IArea> path) {
 		// TODO Auto-generated method stub
-
+		this.path = path;
 	}
 
 	@Override
 	public void analyzePath() {
 		// TODO Auto-generated method stub
-
+		int counter = 0;
+		for(int i = 0; i < getPath().size(); i++){
+			analysis += getPath().get(i).getElevation();
+			counter++;
+		}
 	}
 
 	@Override
