a25427a8724d695f532eefd7918ec91f1e62d564
eriklarson <eriklarson@resnet202-246.wireless.wmich.edu>
2017-03-27 19:45:27 -0400

    added new GUI

--- LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
+++ LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
@@ -1,25 +1,27 @@
 package edu.wmich.cs1120.la5;
 
-import java.io.IOException;
+import edu.wmich.cs1120.la5.TerrainScanner;
+import java.io.*;
+
 
 public class MapCreatorFromDat implements IMapCreator {
-
+	TerrainScanner tc = new TerrainScanner();
 	@Override
 	public void scanTerrain(String fileName, int threshold) throws IOException {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public TerrainScanner getScanner() {
 		// TODO Auto-generated method stub
-		return null;
+		return tc;
 	}
 
 	@Override
 	public void setScanner(TerrainScanner scanner) {
 		// TODO Auto-generated method stub
-
+		tc = scanner;
 	}
 
 }
