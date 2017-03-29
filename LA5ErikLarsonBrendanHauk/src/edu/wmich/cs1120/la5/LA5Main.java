280d6f12e82f4610769c77805150201a53973034
bhauk98 <bhauk98@comcast.net>
2017-03-28 22:53:41 -0400

    javadoc

--- LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/LA5Main.java
+++ LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/LA5Main.java
@@ -2,9 +2,13 @@ package edu.wmich.cs1120.la5;
 
 import edu.wmich.cs1120.la5.gui.MainWindow;
 public class LA5Main {
-
+	/**
+	 * This is the main method that runs everything. It starts the GUI which calls all the files
+	 */
 	public static void main(String[] args) {
+	
 		// TODO Auto-generated method stub
+		//hi
 		MainWindow.startGUI();
 	}
 
--- LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/RadiationAnalyzer.java
+++ LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/RadiationAnalyzer.java
@@ -7,18 +7,28 @@ public class RadiationAnalyzer implements IRover {
 	private ArrayList<IArea> path = new ArrayList<IArea>();
 	private String analysis;
 	
+	/**
+	 * allows other classes to use the path
+	 */
 	@Override
 	public ArrayList<IArea> getPath() {
 		// TODO Auto-generated method stub
 		return path;
 	}
 
+	/**
+	 * sets the path
+	 * @param ArrayList<IArea> path
+	 */
 	@Override
 	public void setPath(ArrayList<IArea> path) {
 		// TODO Auto-generated method stub
 		this.path = path;
 	}
 
+	/**
+	 * this analyzes the path to find the radiation
+	 */
 	@Override
 	public void analyzePath() {
 		// TODO Auto-generated method stub
@@ -30,12 +40,19 @@ public class RadiationAnalyzer implements IRover {
 		analysis = Double.toString(ans);
 	}
 
+	/**
+	 * allows other classes to use the analysis
+	 */
 	@Override
 	public String getAnalysis() {
 		// TODO Auto-generated method stub
 		return analysis;
 	}
 
+	/**
+	 * sets the analyzer
+	 * @param String analysis
+	 */
 	@Override
 	public void setAnalysis(String analysis) {
 		// TODO Auto-generated method stub
