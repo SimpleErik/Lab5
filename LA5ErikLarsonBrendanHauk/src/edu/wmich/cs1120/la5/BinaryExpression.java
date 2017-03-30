6b861e9dc9e79b0c4c914f0c85b766d5f4fc7029
bhauk98 <bhauk98@comcast.net>
2017-03-30 15:17:14 -0400

    javadocs

--- LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/BinaryExpression.java
+++ LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/BinaryExpression.java
@@ -1,30 +1,66 @@
 package edu.wmich.cs1120.la5;
 
-public class BinaryExpression implements IExpression {
+import java.util.ArrayList;
+
+public class ElevationAnalyzer implements IRover {
 	
-	private char op;
-	private int left;
-	private int right;
-	Integer val;
+	private ArrayList<IArea> path = new ArrayList<IArea>();
+	private String analysis;
 	/**
-	 * sets the binary expressions
-	 * @param left
-	 * @param right
-	 * @param op
-	 */
-	public BinaryExpression(int left, int right, char op){
-		this.op = op;
-		this.left = left;
-		this.right = right;
-	}
-	/**
-	 * gets the value
-	 * @param val
+	 * This method is so other classes can get the the value of path
+	 * @return path
 	 */
 	@Override
-	public Integer getValue() {
+	public ArrayList<IArea> getPath() {
 		// TODO Auto-generated method stub
-		return val;
+		return path;
+	}
+	
+	/**
+	 * This is the method that sets the variable path
+	 * @param path
+	 */
+	@Override
+	public void setPath(ArrayList<IArea> path) {
+		// TODO Auto-generated method stub
+		this.path = path;
+	}
+	
+	/**
+	 * This method analyzes the path to see what the elevation of the path is
+	 */
+	@Override
+	public void analyzePath() {
+		// TODO Auto-generated method stub
+		double counter = 0;
+		double ans = 0;
+		analysis = "0.0";
+		double toDouble = Double.parseDouble(analysis);
+		for(int i = 0; i < getPath().size(); i++){
+			toDouble += getPath().get(i).getElevation();
+			counter++;
+		}
+		ans = toDouble / counter;
+		analysis = Double.toString(ans);
+	}
+	/**
+	 * This method is so other classes can be able to call and use the variable analysis 
+	 * @return analysis
+	 */
+	@Override
+	public String getAnalysis() {
+		// TODO Auto-generated method stub
+		return analysis;
+	}
+
+	/**
+	 * This is the method that sets the variable analysis
+	 * @param analysis
+	 */
+	@Override
+	public void setAnalysis(String analysis) {
+		// TODO Auto-generated method stub
+		this.analysis = analysis; 
 	}
 
 }
