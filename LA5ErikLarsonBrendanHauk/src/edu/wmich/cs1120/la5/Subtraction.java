b760f8df7028332057e9dab9653be495c945d3c3
bhauk98 <bhauk98@comcast.net>
2017-03-30 15:33:02 -0400

    docs

--- LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/Subtraction.java
+++ LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/Subtraction.java
@@ -2,6 +2,12 @@ package edu.wmich.cs1120.la5;
 
 public class Subtraction implements IOperation {
 
+	/**
+	 * Subtracts the left and right expressions
+	 * @param left
+	 * @param right
+	 * @return left.getValue() - right.getValue()
+	 */
 	@Override
 	public Integer perform(IExpression left, IExpression right) {
 		// TODO Auto-generated method stub
