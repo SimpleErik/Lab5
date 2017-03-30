84043cedd98b4b79999fb1cbe17bf5ac3b4052dd
bhauk98 <bhauk98@comcast.net>
2017-03-30 15:11:44 -0400

    added java docs

--- LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/Addition.java
+++ LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/Addition.java
@@ -2,6 +2,10 @@ package edu.wmich.cs1120.la5;
 
 public class Addition implements IOperation {
 
+	/**
+	 * adds the left and right expressions
+	 * @return left.getValue() + right.getValue()
+	 */
 	@Override
 	public Integer perform(IExpression left, IExpression right) {
 		// TODO Auto-generated method stub
