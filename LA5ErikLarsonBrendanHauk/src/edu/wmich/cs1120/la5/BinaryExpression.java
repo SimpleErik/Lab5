d86a452cc503e2e3011a5b3dbc3ff533bb467747
bhauk98 <bhauk98@comcast.net>
2017-03-30 15:15:43 -0400

    javadocs

--- LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/BinaryExpression.java
+++ LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/BinaryExpression.java
@@ -6,11 +6,21 @@ public class BinaryExpression implements IExpression {
 	private int left;
 	private int right;
 	Integer val;
+	/**
+	 * sets the binary expressions
+	 * @param left
+	 * @param right
+	 * @param op
+	 */
 	public BinaryExpression(int left, int right, char op){
 		this.op = op;
 		this.left = left;
 		this.right = right;
 	}
+	/**
+	 * gets the value
+	 * @param val
+	 */
 	@Override
 	public Integer getValue() {
 		// TODO Auto-generated method stub
