0ae4b8552e9b32a75a5a55b766e167b13f255d61
bhauk98 <bhauk98@comcast.net>
2017-03-30 15:21:43 -0400

    javadocs

--- LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/ExpressionFactory.java
+++ LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/ExpressionFactory.java
@@ -2,6 +2,13 @@ package edu.wmich.cs1120.la5;
 
 public class ExpressionFactory {
 
+	/**
+	 * allows other classes to get the expressions
+	 * @param operator
+	 * @param val1
+	 * @param val2
+	 * @return i
+	 */
 	public static IExpression getExpression(char operator, int val1, int val2) {
 		IExpression i = new BinaryExpression(val1, val2, operator);
 		return i;
