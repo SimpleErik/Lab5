939b11a6b825a5212ed57f610a1e1e15fef6438a
bhauk98 <bhauk98@comcast.net>
2017-03-30 15:28:00 -0400

    docs

--- LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/Literal.java
+++ LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/Literal.java
@@ -4,9 +4,17 @@ public class Literal implements IExpression {
 	
 	private Integer val;
 	
+	/**
+	 * sets the variable val
+	 * @param value
+	 */
 	public Literal(Integer value){
 		val = value;
 	}
+	/**
+	 * returns the value
+	 * @return val
+	 */
 	@Override
 	public Integer getValue() {
 		// TODO Auto-generated method stub
