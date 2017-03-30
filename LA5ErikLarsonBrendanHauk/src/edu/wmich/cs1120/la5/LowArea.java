fa9cda008c24e0d5688864cb66052000c7065470
bhauk98 <bhauk98@comcast.net>
2017-03-30 15:25:44 -0400

    javadocs

--- LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/LowArea.java
+++ LA5ErikLarsonBrendanHauk/src/edu/wmich/cs1120/la5/LowArea.java
@@ -1,6 +1,6 @@
 package edu.wmich.cs1120.la5;
 
-public class HighArea extends Area {
+public class LowArea extends Area {
 	private double basicEnergyCost;
 	private double elevation;
 	private double radiation;
